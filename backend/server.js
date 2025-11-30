f10deaca9bf2514c5bf4aae173b6f86579db6883
Kim-one <kimonebarrett16@gmail.com>
2025-11-30 00:13:48 -0400

    changed cors

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const GameLobbyModel = require('./models/GameLobbyModel');
 
 
 app.use(cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"]
 }));
